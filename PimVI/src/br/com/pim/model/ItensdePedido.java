df964c480b2b52b113096c2faa06b4c8b6cf5fd3
Pri <priscila@priscila.com.br>
2017-05-27 19:21:20 -0300

    Alterado itens de pedido

--- PimVI/src/br/com/pim/model/ItensdePedido.java
+++ PimVI/src/br/com/pim/model/ItensdePedido.java
@@ -3,9 +3,32 @@ package br.com.pim.model;
 public class ItensdePedido extends Pedido{
 
 	private Estoque e;
+	private Reservar r;
+	private int qtd;
+	private double totalItem;
+	private double totalPedido;
 	
+
 	
+	public double somaItem(Estoque e) {
+		return totalItem = e.getValorUnit() * qtd;	
+	}
 	
+	double validarEstoque(Estoque e){
+		if (qtd<e.getQtde()){
+			System.out.println("Estoque indisponível. Reservar produto?");	
+			r.setUsuario(r.getUsuario());
+			r.setLivro(r.getLivro());
+			r.setDataLimite(r.getDataLimite());
+		}
+		return somaItem(e);
+	}
+	
+	public double totalPedido(Estoque e) {
+		return totalPedido = this.totalItem + totalPedido;	
+	}
+	
+		
 	public ItensdePedido(Livro livro, boolean disponivel, boolean reservar, int qtdDesejada, double valorTotal) {
 		super(livro, disponivel, reservar, qtdDesejada, valorTotal);
 		// TODO Auto-generated constructor stub
@@ -16,6 +39,5 @@ public class ItensdePedido extends Pedido{
 	public void setE(Estoque e) {
 		this.e = e;
 	}
-	
 
 }
